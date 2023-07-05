5c1b2d99ac870fa814dafb8309c906045ea9e611
deividvaz1 <deividvaz100@gmail.com>
2023-07-05 17:04:10 -0300

    preparando para usar a api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 'use client'
+import { useState } from 'react'
 
 import { Input } from '@/components/Input'
 
 export default function Home() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  const [error, setError] = useState('')
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${location}&days=7&aqi=yes&alerts=yes`
   return (
     <div className="h-screen bg-gradient-to-r from-blue-500 to-blue-300 bg-cover">
       <div className="flex h-fit w-full flex-col bg-white/25">
