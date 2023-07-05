69164ce1f3eef84d093b3a13bd07ec88ac3e855f
deividvaz1 <deividvaz100@gmail.com>
2023-07-05 17:31:43 -0300

    alguns erros na hora de fazer a requisição

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import { Input } from '@/components/Input'
 
@@ -8,17 +8,37 @@ export default function Home() {
   const [location, setLocation] = useState('')
   const [error, setError] = useState('')
 
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${location}&days=7&aqi=yes&alerts=yes`
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&days=7&aqi=yes&alerts=yes`
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error()
+        }
+        const data = await response.json()
+        setData(data)
+        setLocation('')
+        setError('')
+      } catch (error) {
+        setError('Cidade não encontrada ou não existe')
+        setData({})
+      }
+    }
+  }
   return (
     <div className="h-screen bg-gradient-to-r from-blue-500 to-blue-300 bg-cover">
       <div className="flex h-fit w-full flex-col bg-white/25">
         {/* LOGO */}
         <div className="flex flex-col  items-center justify-between p-12 md:flex-row">
-          <Input />
+          <Input handleSearch={handleSearch} setLocation={setLocation} />
           <h1 className="order-1 mb-8 rounded-xl px-4 py-2 font-bold italic text-white md:mb-0">
             Clima do Tempo.
           </h1>
         </div>
+        {data.current ? <div>{data.current.temp_c}</div> : null}
       </div>
     </div>
   )
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,20 @@
 'use client'
 import { BiSearchAlt } from 'react-icons/bi'
 
-export function Input() {
+interface InputProps {
+  handleSearch: (event: React.KeyboardEvent<HTMLInputElement>) => void
+  setLocation: React.Dispatch<React.SetStateAction<String>>
+}
+
+export function Input({ handleSearch, setLocation }: InputProps) {
   return (
     <form className="order-2 flex w-full items-center md:order-1 md:w-2/4">
       <input
         type="text"
         placeholder="Encontre sua Cidade"
         className="w-full border-b-2 bg-transparent text-white placeholder-white outline-none"
+        onKeyDown={handleSearch}
+        onChange={(e) => setLocation(e.target.value)}
       />
       <div className="ml-[-25px] cursor-pointer text-white">
         <BiSearchAlt />
