bc4333c146a65fe07942d33b82fb920290e46647
deividvaz1 <deividvaz100@gmail.com>
2023-07-06 17:43:13 -0300

    mais detalhes finalizado, talvez mudanças nos icons

--- src/components/WeatherDetail.tsx
+++ src/components/WeatherDetail.tsx
@@ -12,67 +12,86 @@ export function WeatherDetail({ data }) {
       <div className="p-12">
         <h1 className="mb-4 text-2xl text-white">Mais detalhes</h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {/* Vel. AR */}
           <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
             <div className="text-2xl">
-              <h3>Vel. do Vento</h3>
+              <h3>Vel. do Ar</h3>
               <h3>{data.current.wind_kph} km/h</h3>
             </div>
             <div>
               <GiWindSlap fontSize={40} />
             </div>
           </div>
+
+          {/* Umidade do Ar */}
           <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
             <div className="text-2xl">
-              <h3>Vel. do Vento</h3>
-              <h3>{data.current.wind_kph} km/h</h3>
+              <h3>Umidade do Ar</h3>
+              <h3>{data.current.humidity} %</h3>
             </div>
             <div>
-              <GiWindSlap fontSize={40} />
+              <WiHumidity fontSize={40} />
+            </div>
+          </div>
+
+          {/* Direção do Ar */}
+          <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
+            <div className="text-2xl">
+              <h3>Direção do Vento</h3>
+              <h3>{data.current.wind_dir}</h3>
+            </div>
+            <div>
+              <GiCompass fontSize={40} />
+            </div>
+          </div>
+
+          {/* Nascer do sol */}
+          <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
+            <div className="text-2xl">
+              <h3>Nascer do Sol</h3>
+              <h3>{data.forecast.forecastday[0].astro.sunrise}</h3>
+            </div>
+            <div>
+              <BsSunrise fontSize={40} />
+            </div>
+          </div>
+          {/* Por do sol */}
+          <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
+            <div className="text-2xl">
+              <h3>Por do Sol</h3>
+              <h3>{data.forecast.forecastday[0].astro.sunset}</h3>
+            </div>
+            <div>
+              <BsSunset fontSize={40} />
+            </div>
+          </div>
+
+          {/* Pressão do ar */}
+          <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
+            <div className="text-2xl">
+              <h3>Pressão do ar</h3>
+              <h3>{data.current.pressure_in}</h3>
+            </div>
+            <div>
+              <MdAir fontSize={40} />
             </div>
           </div>
           <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
             <div className="text-2xl">
-              <h3>Vel. do Vento</h3>
-              <h3>{data.current.wind_kph} km/h</h3>
+              <h3>Sensação térmica</h3>
+              <h3>{data.current.feelslike_c}°</h3>
             </div>
             <div>
-              <GiWindSlap fontSize={40} />
+              <CiTempHigh fontSize={40} />
             </div>
           </div>
           <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
             <div className="text-2xl">
-              <h3>Vel. do Vento</h3>
-              <h3>{data.current.wind_kph} km/h</h3>
+              <h3>Visibilidade</h3>
+              <h3>{data.current.vis_km} km< /h3>
             </div>
             <div>
-              <GiWindSlap fontSize={40} />
-            </div>
-          </div>
-          <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
-            <div className="text-2xl">
-              <h3>Vel. do Vento</h3>
-              <h3>{data.current.wind_kph} km/h</h3>
-            </div>
-            <div>
-              <GiWindSlap fontSize={40} />
-            </div>
-          </div>
-          <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
-            <div className="text-2xl">
-              <h3>Vel. do Vento</h3>
-              <h3>{data.current.wind_kph} km/h</h3>
-            </div>
-            <div>
-              <GiWindSlap fontSize={40} />
-            </div>
-          </div>
-          <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
-            <div className="text-2xl">
-              <h3>Vel. do Vento</h3>
-              <h3>{data.current.wind_kph} km/h</h3>
-            </div>
-            <div>
-              <GiWindSlap fontSize={40} />
+              <FaEye fontSize={40} />
             </div>
           </div>
         </div>
