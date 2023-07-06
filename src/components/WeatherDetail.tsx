51a0cc0f57c52e650b891f046bf96d3de7ddf039
deividvaz1 <deividvaz100@gmail.com>
2023-07-06 17:26:19 -0300

    Parte da velocidade do vento criada

--- src/components/WeatherDetail.tsx
+++ src/components/WeatherDetail.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { BsSunrise, BsSunset } from 'react-icons/bs'
+import { GiWindSlap, GiCompass } from 'react-icons/gi'
+import { WiHumidity } from 'react-icons/wi'
+import { MdAir } from 'react-icons/md'
+import { CiTempHigh } from 'react-icons/ci'
+import { FaEye } from 'react-icons/fa'
 
 export function WeatherDetail({ data }) {
   return (
@@ -6,8 +12,14 @@ export function WeatherDetail({ data }) {
       <div className="p-12">
         <h1 className="mb-4 text-2xl text-white">Mais detalhes</h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          <div className="flexp-4 items-center justify-between gap-6 rounded-xl bg-white/50">
-            <div className="text-2xl"></div>
+          <div className="flex items-center justify-between gap-6 rounded-xl bg-white/50 p-4">
+            <div className="text-2xl">
+              <h3>Vel. do Vento</h3>
+              <h3>{data.current.wind_kph} km/h</h3>
+            </div>
+            <div className="">
+              <GiWindSlap fontSize={40} />
+            </div>
           </div>
         </div>
       </div>
